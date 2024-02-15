ebeecf02f03391c8f5e19b92add33422edc5d359
Chris Zioutas <chris.zioutas@cozero.io>
2024-02-15 11:27:28 +0200

    example

--- src/logger/app-logger.service.ts
+++ src/logger/app-logger.service.ts
@@ -6,11 +6,33 @@ export class AppLogger extends ConsoleLogger {
     super(context)
   }
 
-  log(message: any, context?: string): void {
-    super.log(message, context);
+  log(message: any, context?: string) {
+    const modifiedMessage = `${message} test`;
+
+    console.log(modifiedMessage, context);
+
+    super.log(modifiedMessage, context);
   }
 
-  error(message: any, context?: string): void {
-    super.error(message, context);
+  error(message: any, stack?: string, context?: string) {
+    const modifiedMessage = `${message} test`;
+
+    console.log(modifiedMessage, stack, context);
+
+    super.error(modifiedMessage, stack, context);
+
+    // if super.error(modifiedMessage, stack, context);
+
+    // [Nest] 84265  - 02/15/2024, 9:54:28 AM   ERROR [AppController] error test
+    // [Nest] 84265  - 02/15/2024, 9:54:28 AM   ERROR [AppController] undefined  <-- where is it coming from?
+
+    // with the test bellow it confirms that stack trace is the 2nd log
+    // however as before it is undefined, it should not be outputed. as the stack argument is optional
+    // if super.error(modifiedMessage, { "hey": "you" }, context);
+    // [Nest] 84387  - 02/15/2024, 9:55:05 AM   ERROR [AppController] error test
+    // [Nest] 84387  - 02/15/2024, 9:55:05 AM   ERROR [AppController] Object:
+    // {
+    //   "hey": "you"
+    // }
   }
 }
